1faa32adb7605d96598df02b66b96374768be4e6
Gabriel Gradinar <gabriel.gradinar@pontotel.com.br>
2020-06-20 15:08:46 -0300

    Implementando o padrao de Factory

--- delivery/app.py
+++ delivery/app.py
@@ -1,12 +1,11 @@
+import views
 from flask import Flask
 
-app = Flask(__name__)
-# __name__ = nome do módulo
-
-@app.route("/")
-def index():
-    return "<h1>Hello World</h1>"
-
-@app.route("/sobre")
-def sobre():
-    return "<h1>Melhor site de delivery !!!</h1>"
+def create_app():
+    """
+        Factory Principal
+    """
+    app = Flask(__name__)
+    views.init_app(app)
+    # __name__ = nome do módulo
+    return app

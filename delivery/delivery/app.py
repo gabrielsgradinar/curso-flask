05b29820831ba56fe8e89ecc2b465eb9aa098eb8
Gabriel Gradinar <gabriel.gradinar@pontotel.com.br>
2020-07-04 17:27:49 -0300

    Add templates and debug tool bar

--- delivery/delivery/app.py
+++ delivery/delivery/app.py
@@ -1,11 +1,16 @@
 from flask import Flask
+
 from delivery.ext import site
+from delivery.ext import toolbar
+from delivery.ext import config
 
 def create_app():
     """
         Factory Principal
     """
     app = Flask(__name__)
+    config.init_app(app)
+    toolbar.init_app(app)
     site.init_app(app)
     # __name__ = nome do módulo
     return app
---
+++ delivery/delivery/ext/config/__init__.py
@@ -0,0 +1,3 @@
+
+def init_app(app):
+    app.config["SECRET_KEY"]="1705"

8fbd3f6951185b62806396332f98f5cc26610134
Gabriel Gradinar <gabriel.gradinar@pontotel.com.br>
2020-08-10 20:10:02 -0300

    Isort and black

--- delivery/delivery/app.py
+++ delivery/delivery/app.py
@@ -1,15 +1,16 @@
 from flask import Flask
 
-from delivery.ext import site
-from delivery.ext import config
-from delivery.ext import toolbar
-from delivery.ext import db
-from delivery.ext import migrate
-from delivery.ext import cli
-from delivery.ext import hooks
-from delivery.ext import auth
-from delivery.ext import admin
-
+from delivery.ext import (
+    admin,
+    auth,
+    cli,
+    config,
+    db,
+    hooks,
+    migrate,
+    site,
+    toolbar,
+)
 
 
 def create_app():

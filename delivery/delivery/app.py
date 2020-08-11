639862c1262d48489a336db8367042ed24131cf8
Gabriel Gradinar <gabriel.gradinar@pontotel.com.br>
2020-08-10 22:08:36 -0300

    Install and config dynaconf

--- delivery/delivery/app.py
+++ delivery/delivery/app.py
@@ -1,16 +1,6 @@
 from flask import Flask
 
-from delivery.ext import (
-    admin,
-    auth,
-    cli,
-    config,
-    db,
-    hooks,
-    migrate,
-    site,
-    toolbar,
-)
+from delivery.ext import config
 
 
 def create_app():
@@ -19,13 +9,14 @@ def create_app():
     """
     app = Flask(__name__)
     config.init_app(app)
-    db.init_app(app)
-    auth.init_app(app)
-    admin.init_app(app)
-    migrate.init_app(app)
-    cli.init_app(app)
-    toolbar.init_app(app)
-    site.init_app(app)
-    hooks.init_app(app)
+
+    # db.init_app(app)
+    # auth.init_app(app)
+    # admin.init_app(app)
+    # migrate.init_app(app)
+    # cli.init_app(app)
+    # toolbar.init_app(app)
+    # site.init_app(app)
+    # hooks.init_app(app)
     # __name__ = nome do módulo
     return app
--- delivery/delivery/ext/config/__init__.py
+++ delivery/delivery/ext/config/__init__.py
@@ -1,11 +1,5 @@
+from dynaconf import FlaskDynaconf
+
 def init_app(app):
-    app.config["SECRET_KEY"] = "1705"
-    app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///delivery.db'
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    app.config["FLASK_ADMIN_SWATCH"] = "slate"
-
-
-    if app.debug:
-        app.config['DEBUG_TB_TEMPLATE_EDITOR_ENABLED'] = True
-        app.config['DEBUG_TB_PROFILER_ENABLED'] = True
-        app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+    FlaskDynaconf(app)
+    app.config.load_extensions("EXTENSIONS")
